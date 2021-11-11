48f7ee1ab59de88a66dc1f554b661bb3f942ebc8
andersontrkz <trkz.anderson@gmail.com>
2021-11-11 12:58:45 -0300

    build(login): add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
+import Login from './components/Pages/Login/Login';
 
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Login />
 );
 
 export default App;
---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+const Logo = () => (
+  <>
+    <Text fontWeight="bold" fontSize="5xl" color="var(--primary-color)">SOS</Text>
+    <Text fontWeight="bold" fontSize="5xl" color="var(--secondary-color)">Produtor</Text>
+  </>
+);
+
+export default Logo;
