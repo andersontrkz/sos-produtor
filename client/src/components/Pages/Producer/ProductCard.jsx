bb3a14aa64dd68b7f45e98efc337219a7ca1b223
andersontrkz <trkz.anderson@gmail.com>
2021-11-18 13:42:39 -0300

    style(producer page): producer products cards

--- client/src/components/Pages/Producer/ProductCard.jsx
+++ client/src/components/Pages/Producer/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
-  GridItem, Button, Input, Flex, Text, Image, Badge, Box,
+  Grid, GridItem, Button, Input, Flex, Text, Image, Badge,
 } from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -58,46 +58,50 @@ const ProductCard = ({
   }, [cart]);
 
   return (
-    <GridItem w="60" h="72" maxW="60" maxH="72" boxShadow="xl" bg="#FFF" _hover={{ transform: 'scale(1.05)', transition: '.9s' }}>
-      <Box h="19px">
-        {benefits?.free_delivery && (
-        <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="whatsapp" display="flex" alignItems="center">
-          Frete Grátis
-        </Badge>
-        ) }
-        {benefits?.discount && (
-        <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="red" display="flex" alignItems="center">
-          {`${benefits?.discount} OFF`}
-        </Badge>
-        )}
-        {benefits?.new && (
-        <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="linkedin" display="flex" alignItems="center">
-          Novo
-        </Badge>
-        )}
-      </Box>
-      <Flex h="36">
-        <Image src={image} m="0 auto" objectFit="cover" />
-      </Flex>
-      <Flex flexDirection="column" justify="center" textAlign="center">
-        <Text maxH="50px" overflowY="auto">
-          {`${name} - ${measurement.amount + measurement.unit}`}
-        </Text>
-        <Text>
-          {`R$ ${price.toFixed(2)}`}
-        </Text>
-        <Flex p={4}>
-          <Button colorScheme="whatsapp" borderRadius="8px 0 0 8px" onClick={() => handleProductCart(-1)}>-</Button>
-          <Input
-            borderRadius={0}
-            textAlign="center"
-            value={inputQuantity}
-            onChange={({ target }) => handleQuantityInput(target)}
-            onBlur={({ target }) => onFocusOutQuantityInput(target)}
-          />
-          <Button colorScheme="whatsapp" borderRadius="0 8px 8px 0" onClick={() => handleProductCart(1)}>+</Button>
-        </Flex>
-      </Flex>
+    <GridItem w="60" h="80" maxW="60" maxH="80" boxShadow="xl" borderRadius="8px" bg="#FFF" _hover={{ transform: 'scale(1.05)', transition: '.9s' }}>
+      <Grid templateColumns="repeat(12, 1fr)">
+        <GridItem colSpan={12} minH="19px" maxH="19px">
+          {benefits?.free_delivery && (
+          <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="whatsapp" display="flex" alignItems="center">
+            Frete Grátis
+          </Badge>
+          ) }
+          {benefits?.discount && (
+          <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="red" display="flex" alignItems="center">
+            {`${benefits?.discount} OFF`}
+          </Badge>
+          )}
+          {benefits?.new && (
+          <Badge my={2} px={2} borderRadius="0 8px 8px 0" position="relative" zIndex="999" w="fit-content" fontSize="xs" colorScheme="linkedin" display="flex" alignItems="center">
+            Novo
+          </Badge>
+          )}
+        </GridItem>
+        <GridItem colSpan={12} h="40" display="flex">
+          <Image src={image} m="0 auto" objectFit="cover" />
+        </GridItem>
+        <GridItem mt={1} colSpan={12} display="flex" flexDirection="column" justify="center" textAlign="center">
+          <Text minH="44px" maxH="44px" overflowY="auto" fontSize="sm">
+            {`${name} - ${measurement.amount + measurement.unit}`}
+          </Text>
+          <Text>
+            {`R$ ${price.toFixed(2)}`}
+          </Text>
+        </GridItem>
+        <GridItem mt={1} colSpan={12} display="flex" flexDirection="column" justify="center" textAlign="center">
+          <Flex p={3}>
+            <Button colorScheme="whatsapp" borderRadius="8px 0 0 8px" onClick={() => handleProductCart(-1)}>-</Button>
+            <Input
+              borderRadius={0}
+              textAlign="center"
+              value={inputQuantity}
+              onChange={({ target }) => handleQuantityInput(target)}
+              onBlur={({ target }) => onFocusOutQuantityInput(target)}
+            />
+            <Button colorScheme="whatsapp" borderRadius="0 8px 8px 0" onClick={() => handleProductCart(1)}>+</Button>
+          </Flex>
+        </GridItem>
+      </Grid>
     </GridItem>
   );
 };
