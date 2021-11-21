e8b059314895d62ac5ac33684f4caea6a2611e4a
andersontrkz <trkz.anderson@gmail.com>
2021-11-21 01:13:23 -0300

    chore(register page): validate inputs

--- client/src/components/Pages/Register/Register.jsx
+++ client/src/components/Pages/Register/Register.jsx
@@ -12,6 +12,7 @@ import Layout from '../../Layout/Layout';
 const Register = () => {
   const history = useHistory();
   const dispatch = useDispatch();
+  const [errorMessage, setErrorMessage] = useState(false);
   const [producer, setProducer] = useState({
     name: '',
     email: '',
@@ -25,10 +26,48 @@ const Register = () => {
     setProducer({ ...producer, [id]: value });
   };
 
+  const validateForm = () => {
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    const nameRegex = /^[a-z ,.'-]+$/i;
+
+    if (producer.name === '' || producer.email === '' || producer.ddd === '' || producer.phone === '' || producer.password === '' || producer.confirmPassword === '') {
+      setErrorMessage('Preencha todos os campos*');
+      return false;
+    }
+
+    if (!nameRegex.test(producer.name)) {
+      setErrorMessage('Preencha um nome válido*');
+      return false;
+    }
+
+    if (!emailRegex.test(producer.email)) {
+      setErrorMessage('Preencha um email válido*');
+      return false;
+    }
+
+    if (producer.phone.length < 8) {
+      setErrorMessage('Preencha um telefone válido*');
+      return false;
+    }
+
+    if (producer.password !== producer.confirmPassword) {
+      setErrorMessage('As senhas precisam ser iguais*');
+      return false;
+    }
+
+    if (producer.password.length < 8) {
+      setErrorMessage('Sua senha precisa ter pelo menos 8 caracteres*');
+      return false;
+    }
+    return true;
+  };
+
   const redirectToHome = () => {
-    dispatch(postProducerAction(producer));
-    dispatch(setCustomerAction(producer));
-    history.push('/');
+    if (validateForm()) {
+      dispatch(postProducerAction(producer));
+      dispatch(setCustomerAction(producer));
+      history.push('/');
+    }
   };
 
   return (
@@ -51,16 +90,19 @@ const Register = () => {
               <Input id="email" value={producer.email} placeholder="Email" onChange={({ target }) => handleInput(target)} />
             </GridItem>
             <GridItem colSpan={3}>
-              <Input id="ddd" value={producer.ddd} placeholder="DDD" onChange={({ target }) => handleInput(target)} />
+              <Input id="ddd" type="number" value={producer.ddd} placeholder="DDD" onChange={({ target }) => handleInput(target)} />
             </GridItem>
             <GridItem colSpan={9}>
-              <Input id="phone" value={producer.phone} placeholder="Telefone" onChange={({ target }) => handleInput(target)} />
+              <Input id="phone" type="number" value={producer.phone} placeholder="Telefone" onChange={({ target }) => handleInput(target)} />
             </GridItem>
             <GridItem colSpan={6}>
-              <Input id="password" value={producer.password} placeholder="Senha" onChange={({ target }) => handleInput(target)} />
+              <Input id="password" type="password" value={producer.password} placeholder="Senha" onChange={({ target }) => handleInput(target)} />
             </GridItem>
             <GridItem colSpan={6}>
-              <Input id="confirmPassword" value={producer.confirmPassword} placeholder="Confirme a Senha" onChange={({ target }) => handleInput(target)} />
+              <Input id="confirmPassword" type="password" value={producer.confirmPassword} placeholder="Confirme a Senha" onChange={({ target }) => handleInput(target)} />
+            </GridItem>
+            <GridItem colSpan={12}>
+              {errorMessage && <Text textAlign="center" fontSize="xs" cursor="pointer" transition=".9s" _hover={{ color: 'var(--quaternary-color)' }}>{errorMessage}</Text>}
             </GridItem>
             <GridItem colSpan={12}>
               <Button
